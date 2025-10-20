9b2c54465fc1e9c5079675ad6b2710e97a560351
Jeff McDonald <jwmcdonald85@gmail.com>
2025-10-20 17:29:21 -0400

    NavItems created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Click Me</Button>
+    <div className="flex min-h-screen home-wrapper">
+      {/* <Button>Click Me</Button> */}
+      Home
     </div>
   )
 }
